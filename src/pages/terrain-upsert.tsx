abf747c85c589946140ba6b6975a61587549a675
i4c0ni99 <i4c0ni99@gmail.com>
2024-05-16 23:48:14 +0200

    last commit

--- src/pages/terrain-upsert.tsx
+++ src/pages/terrain-upsert.tsx
@@ -2,14 +2,14 @@ import { CreateEditTerrain } from "../components/create-edit-terrain/create-edit
 import { User } from "../types/User.class";
 import { Terrain } from "../types/terrain.class";
 export function TerrainUpsert() {
-    const user =   new User(
+    const user = new User(
         'i4c0ni99@gmail.com',
         '',
         'Gigi',
         'Iaconi',
         'i4c0ni99')
 
-    
+
     return (<>
         <div className="size-3/4 mx-auto">
             <CreateEditTerrain
@@ -22,8 +22,8 @@ export function TerrainUpsert() {
                         0,
                         false,
                         user,
-                        new Array()
-                    )   
+                        []
+                    )
                 }
                 onSubmission={(data: Terrain) => console.log("AA", data)}>
             </CreateEditTerrain>

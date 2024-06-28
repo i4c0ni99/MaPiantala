e28813dc00a3642458a502b7385c5e21c815eef6
i4c0ni99 <i4c0ni99@gmail.com>
2024-06-28 12:14:54 +0200

    ultimo

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -9,7 +9,7 @@ export function RegistrationPage() {
 
   return (
     <HeroRegister
-      user={new User("", "", "", "", "", "", false, "","")}
+      user={new User(1,"", "", "", "", "", "", false, "","")}
       onSubmission={async (data:User) =>{
         setuserPost({firstname: data.firstName,lastname: data.lastName,email: data.email,password : data.password,passworConfirm: data.passwordConfirm})
         await ky.post('http://localhost:3000/user/signup',{json:userpost})

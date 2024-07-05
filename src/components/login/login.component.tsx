2784b8cf98a6076d881fc9091c02bed757879549
i4c0ni99 <i4c0ni99@gmail.com>
2024-07-05 13:02:06 +0200

    aggiunta axios

--- src/components/login/login.component.tsx
+++ src/components/login/login.component.tsx
@@ -1,38 +1,35 @@
 import { useEffect, useState } from "react";
 import { HeroLogin } from "./loginCard.component";
 import ky from 'ky';
+import { axiosInstance } from "../../utils/axiosInstance";
 
 export const LoginModal = function () {
-const [passName,setPassName] = useState ({})
-    useEffect(() => {
-         ky.post('http://localhost:3000/user/signin',{json : passName})
-      }, [passName]);
-  return (
-    <>
-      <li>
-        <a
-          onClick={() =>
-            (
-              document.getElementById("my_modal_3") as HTMLDialogElement
-            ).showModal()
-          }
-        >
-          login
-        </a>
-      </li>
 
-      <dialog id="my_modal_3" className=" modal w-3/4 h-full mx-auto">
-        <div className="w-full h-auto ">
-          <HeroLogin
-            onSubmission={async (data) =>
-            {
-            setPassName(data)
-            console.log(data)
-            }
-            }
-          />
-        </div>
-      </dialog>
-    </>
-  );
+    return (
+        <>
+            <li>
+                <a
+                    onClick={() =>
+                        (
+                            document.getElementById("my_modal_3") as HTMLDialogElement
+                        ).showModal()
+                    }
+                >
+                    login
+                </a>
+            </li>
+
+            <dialog id="my_modal_3" className=" modal w-3/4 h-full mx-auto">
+                <div className="w-full h-auto ">
+                    <HeroLogin
+                        onSubmission={async (data) => {
+                            const respone = await axiosInstance.post('http://localhost:3000/user/signin', data)
+                            console.log(respone)
+                        }
+                        }
+                    />
+                </div>
+            </dialog>
+        </>
+    );
 };

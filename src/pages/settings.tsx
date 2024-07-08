96b3298dc5c7d31aede41b10d8499ccb63410aef
rasta4000 <filipporastelli02@gmail.com>
2024-07-08 11:13:25 +0200

    ultime modifiche

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,17 +1,17 @@
-import { useContext } from "react";
+
 import { Profile } from "../components/profile/profile.component";
-import { MyContext } from "../services/MyContext";
+
 import { User } from "../types/User.class";
 import { HeroSettings } from "../components/settings-profile/settings-profile.component";
+import { getCookie } from "../services/MaPiantalaCookies.service";
 //import axios from "axios";
 
 export function SettingsPage() {
-  const userContext = useContext(MyContext).data.user
 
   return (
     <>
       <div className="h-96 w-full">
-        <Profile user={userContext}></Profile>
+        <Profile user={getCookie('user')}></Profile>
       </div>
 
       <HeroSettings
@@ -19,10 +19,10 @@ export function SettingsPage() {
         console.log(data.email)
         console.log(data.password)
        
-        //await axios.post('http://localhost:3000/user/signin',{json: {firstName: data.firstName, lastName: data.lastName, username: data.username, email: data.email, password : data.password, passwordConfirm : data.passwordConfirm, profilePicture : data.profilePicture, copertinePicture: data.copertinePicture}})
+        //await axios.post('/user/signin',{firstName: data.firstName, lastName: data.lastName, email: data.email, password : data.password, passwordConfirm : data.passwordConfirm, profilePicture : data.profilePicture, copertinePicture: data.copertinePicture})
 
       }  }
-      oldUser={userContext}
+      oldUser={getCookie('user')}
     />
     </>
   );
